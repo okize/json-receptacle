652f6e5a126847903cd4f5f4f3e116ac39d7cdba
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-01-27 18:55:50 -0500

    updates index

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var port = process.env.PORT || 8000;
 
 // app.use(express.json());
 
 app.configure(function(){
   app.use('/public', express.static(__dirname + '/public'));
   app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res) {
+  res.send('nothing to see...');
 });
 
 app.post('/', function(req, res) {
@@ -24,4 +29,5 @@ app.post('/', function(req, res) {
   });
 });
 
-app.listen(process.env.PORT || 8000);
+app.listen(port);
+console.log('listening on port ' + port);
